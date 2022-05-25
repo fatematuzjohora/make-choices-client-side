deea70ae60d23688837dc30dd81ed535421c7437
fatematuzjohora <fatematuzjohora889@gmail.com>
2022-05-26 03:35:54 +0600

    payment-system-added

--- src/Pages/Dashboard/MyReview.js
+++ src/Pages/Dashboard/MyReview.js
@@ -1,31 +1,31 @@
-// import React, { useEffect, useState } from "react";
-// import ReviewCard from "../Home/Reviews/Review/Review";
+import React, { useEffect, useState } from "react";
+import ReviewCard from "../Home/Reviews/Review/Review";
 
-// const MyReview = () => {
-//   const [reviews, setReviews] = useState([0]);
+const MyReview = () => {
+  const [reviews, setReviews] = useState([0]);
 
-//   useEffect(() => {
-//     fetch(" http://localhost:5000/review")
-//       .then((res) => res.json())
-//       .then((data) => {
-//         setReviews(data);
-//       });
-//   }, []);
-//   return (
-//     <div className="mt-[200px] p-12">
-//       <h2 className="text-3xl text-center mb-28 underline">Reviews</h2>
+  useEffect(() => {
+    fetch(" http://localhost:5000/review")
+      .then((res) => res.json())
+      .then((data) => {
+        setReviews(data);
+      });
+  }, []);
+  return (
+    <div className="mt-[200px] p-12">
+      <h2 className="text-3xl text-center mb-28 underline">Reviews</h2>
 
-//       <div
-//         className="grid lg:grid-cols-3 grid-cols-1 md:grid-cols-2 gap-12"
-//         data-aos="fade-up"
-//         data-aos-anchor-placement="center-bottom"
-//       >
-//         {reviews.map((review) => (
-//           <ReviewCard key={review.id} review={review}></ReviewCard>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
+      <div
+        className="grid lg:grid-cols-3 grid-cols-1 md:grid-cols-2 gap-12"
+        data-aos="fade-up"
+        data-aos-anchor-placement="center-bottom"
+      >
+        {reviews.map((review) => (
+          <ReviewCard key={review.id} review={review}></ReviewCard>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-// export default MyReview;
+export default MyReview;
